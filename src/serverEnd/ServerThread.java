d11aa82cbdee1b0eace45bd12853c37196db9c15
apoorva-21 <apoorvagokhale89@gmail.com>
2018-02-09 01:48:35 +0530

    added the modded server end classes

--- src/serverEnd/ServerThread.java
+++ src/serverEnd/ServerThread.java
@@ -5,53 +5,71 @@ import java.util.ArrayList;
 
 import calculatorLogic.BusinessLogic;
 
-
 public class ServerThread extends Thread {
-	Socket currentThreadSocket;
-	String currentClientID;
-	ArrayList <String> calculations = new ArrayList<String>();
-	public ServerThread(Socket socket,int count)
+	private Socket currentThreadSocket;
+	private String currentClientID;
+	private ArrayList <String> clientHistory = new ArrayList<String>();
+	private DataInputStream dIn;
+	private DataOutputStream dOut;
+	
+	public ServerThread(Socket socket,int count) throws IOException
 	{
-		currentThreadSocket = socket;
-		currentClientID = "Client"+ count;
+		this.currentThreadSocket = socket;
+		this.currentClientID = "Client"+ count;
+		this.dOut = new DataOutputStream(this.currentThreadSocket.getOutputStream());
+		this.dIn = new DataInputStream(this.currentThreadSocket.getInputStream());
 	}
+	
+	public void printClientHistory() throws IOException
+	{
+		if(clientHistory.size() > 0)
+		{
+			dOut.writeUTF(currentClientID +", your calculation history :");
+			
+			for(int i = 0; i < clientHistory.size(); i++)
+				dOut.writeUTF(clientHistory.get(i));
+			
+			dOut.writeUTF("fin");
+			
+			dOut.writeUTF(currentClientID +", you have been disconnected!");
+		}
+	}
+	
+	public void handleClientIO() throws IOException
+	{
+		dOut.flush();
+		String clientInput = "";
+		BusinessLogic solver = new BusinessLogic();
+		while(!clientInput.equals("exit")){
+			if((clientInput = dIn.readUTF()) != null)
+			{
+				if(clientInput.equals("exit"))
+					break;
+				String result = solver.solve(clientInput);
+				clientHistory.add(clientInput + " = " + result);
+				dOut.writeUTF("Result = " + result);
+			}
+		}
+	}
+	
+	public void releaseResources() throws IOException
+	{
+		dIn.close();
+		dOut.close();
+		currentThreadSocket.close();
+	}
+	
 	public void run()
 	{
 		try
 		{
 			//create streams for data flow between client-server
-			DataInputStream dIn = new DataInputStream(currentThreadSocket.getInputStream());
-			DataOutputStream dOut = new DataOutputStream(currentThreadSocket.getOutputStream());
-			dOut.flush();
-			String clientInput = "";
-			ArrayList <String> clientHistory = new ArrayList<String>();
-			BusinessLogic solver = new BusinessLogic();
-			while(!clientInput.equals("exit")){
-				if((clientInput = dIn.readUTF()) != null)
-				{
-					if(clientInput.equals("exit"))
-						break;
-					String result = solver.solve(clientInput);
-					clientHistory.add(clientInput + " = " + result);
-					dOut.writeUTF("Result = " + result);
-				}
-			}
-			if(clientHistory.size() > 0)
-			{
-				dOut.writeUTF(currentClientID +", your calculation history :");
-				
-				for(int i = 0; i < clientHistory.size(); i++)
-					dOut.writeUTF(clientHistory.get(i));
-				
-				dOut.writeUTF("fin");
-			}
-			dOut.writeUTF(currentClientID +", you have been disconnected!");
-			dIn.close();
-			dOut.close();
-			currentThreadSocket.close();
+			handleClientIO();
+			printClientHistory();
+			releaseResources();
 		}catch(Exception e)
 		{
-			System.out.println(e.toString());
+			e.printStackTrace();
 		}
 	}
 }
